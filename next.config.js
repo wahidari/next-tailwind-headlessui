a49fe452811e5b8168c59ba9c0b6999a6dda5d9d
wahidari <wahiid.ari@gmail.com>
2022-03-16 23:51:42 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    env: {
+        API_ROUTE: "http://127.0.0.1:8000",
+        // "https://virtserver.swaggerhub.com/masdimya/masdimyaproject1/1.0.0",
+    },
     reactStrictMode: true,
     images: {
         domains: ["dummyimage.com"],
