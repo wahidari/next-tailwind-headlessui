f3511c1b573ff8d953f9b2c7619d5334be55876e
wahidari <wahiid.ari@gmail.com>
2022-03-17 22:32:54 +0700

    add ButtonOutline component

---
+++ components/ButtonOutline.js
@@ -0,0 +1,89 @@
+export default function ButtonOutline({ className, type, value, onClick, disabled, children, ...rest }) {
+	return (
+		<button
+			{...rest}
+			type={type}
+			onClick={onClick}
+			value={value}
+			className={`
+				text-sm text-blue-500 transition-all outline-none px-4 py-2 rounded font-medium border border-blue-500 
+				${className ? className + " " : ""}
+				${disabled ? "cursor-not-allowed" : "hover:text-white hover:bg-blue-500"} 
+				`}
+		>
+			{children}
+		</button>
+	);
+}
+
+ButtonOutline.green = ({ className, type, value, onClick, disabled, children, ...rest }) => {
+	return (
+		<button
+			{...rest}
+			type={type}
+			onClick={onClick}
+			value={value}
+			className={`
+				text-sm text-emerald-500 transition-all outline-none px-4 py-2 rounded font-medium border border-emerald-500
+				${className ? className + " " : ""}
+				${disabled ? "cursor-not-allowed" : "hover:text-white hover:bg-emerald-500"} 
+				`}
+		>
+			{children}
+		</button>
+	);
+}
+
+ButtonOutline.yellow = ({ className, type, value, onClick, disabled, children, ...rest }) => {
+	return (
+		<button
+			{...rest}
+			type={type}
+			onClick={onClick}
+			value={value}
+			className={`
+				text-sm text-amber-500 transition-all outline-none px-4 py-2 rounded font-medium border border-amber-500
+				${className ? className + " " : ""}
+				${disabled ? "cursor-not-allowed" : "hover:text-white hover:bg-amber-500"} 
+				`}
+		>
+			{children}
+		</button>
+	);
+}
+
+ButtonOutline.red = ({ className, type, value, onClick, disabled, children, ...rest }) => {
+	return (
+		<button
+			{...rest}
+			type={type}
+			onClick={onClick}
+			value={value}
+			className={`
+				text-sm text-red-500 transition-all outline-none px-4 py-2 rounded font-medium border border-red-500
+				${className ? className + " " : ""}
+				${disabled ? "cursor-not-allowed" : "hover:text-white hover:bg-red-500"} 
+				`}
+		>
+			{children}
+		</button>
+	);
+}
+
+ButtonOutline.dark = ({ className, type, value, onClick, disabled, children, ...rest }) => {
+	return (
+		<button
+			{...rest}
+			type={type}
+			onClick={onClick}
+			value={value}
+			className={`
+				text-sm text-gray-500 dark:text-gray-300 transition-all outline-none px-4 py-2 rounded font-medium border border-gray-500
+				${className ? className + " " : ""}
+				${disabled ? "cursor-not-allowed" : "hover:text-white hover:bg-gray-500"} 
+				`}
+		>
+			{children}
+		</button>
+	);
+}
