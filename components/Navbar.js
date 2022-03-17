a0fb5a43b396351d14d003f531d379db1804bdc0
Wahid Ari <wahiid.ari@gmail.com>
2022-03-17 18:43:46 +0000

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -103,7 +103,7 @@ export default function Navbar() {
                 </ActiveLink>
                 <ActiveLink activeClassName="bg-gray-800 text-white" href="/second">
                   <a className="border-b-1 block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:bg-gray-700 hover:text-white">Second</a>
-                </ActiveLink
+                </ActiveLink>
 
                 <Menu>
                   {({ open }) => (
