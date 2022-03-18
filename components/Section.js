2b6455b0c77009f9399337743912434754a874ac
wahidari <wahiid.ari@gmail.com>
2022-03-18 21:34:35 +0700

    add section component

--- components/Section.js
+++ components/Section.js
@@ -1,19 +1,9 @@
 import Link from "next/link";
-import Badge from "@components/Badge";
 import Heading from "@components/Heading";
-export default function Section({
-  id,
-  name,
-  children,
-}) {
+export default function Section({ id, name = "", className, children}) {
   return (
-    <section id={id} className="pt-16">
-      <Heading.h3 className="group transition-all duration-500">
-        <span className="transition-all duration-500 text-neutral-500 group-hover:text-black dark:group-hover:text-white">
-          #
-        </span>{" "}
-        <Link href={`#${id}`}>{name}</Link>
-      </Heading.h3>
+    <section id={id} className={`${className ? className + "": ""} pt-10`}>
+      <Heading.h3 className="italic">{name}</Heading.h3>
       <div className="mb-2">{children}</div>
     </section>
   );
