e8ebbf535903df16ee7076aed5558ebea92c9742
wahidari <wahiid.ari@gmail.com>
2022-03-19 10:58:08 +0700

    update Container component

--- components/Container.js
+++ components/Container.js
@@ -1,11 +1,11 @@
-export default function Container({ className, small, children, ...rest }) {
+export default function Container({ className, small, medium, large, children, ...rest }) {
   return (
     <div
       {...rest}
       className={`
         ${className ? className + " " : ""} 
-        ${small ? "p-2" : "p-8"} 
-        relative mb-2 rounded-md border dark:border-neutral-700 bg-white dark:bg-neutral-800`}
+        ${small ? "p-2" : medium ? "p-4" : large ? "p-6" : "p-8"} 
+        relative mb-2 rounded-md border border-slate-200 dark:border-neutral-900 bg-white dark:bg-neutral-800`}
     >
       {children}
     </div>
