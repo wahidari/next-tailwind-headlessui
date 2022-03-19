f57b08440bb524c6220ffcf0482f34e3d63a76eb
wahidari <wahiid.ari@gmail.com>
2022-03-19 10:58:22 +0700

    add LinkButton components

---
+++ components/LinkButton.js
@@ -0,0 +1,113 @@
+import Link from "next/link";
+
+export default function LinkButton({ className, href, pills, children, ...rest }) {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium bg-blue-500 hover:bg-blue-600 text-white`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.green = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium bg-emerald-500 hover:bg-emerald-600 text-white`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.yellow = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest} 
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium bg-amber-500 hover:bg-amber-600 text-white`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.orange = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium bg-orange-500 hover:bg-orange-600 text-white`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.red = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium bg-red-500 hover:bg-red-600 text-white`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.purple = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium bg-violet-500 hover:bg-violet-600 text-white`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.dark = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium bg-gray-500 hover:bg-gray-600 text-white`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
---
+++ components/LinkButtonOutline.js
@@ -0,0 +1,113 @@
+import Link from "next/link";
+
+export default function LinkButton({ className, href, pills, children, ...rest }) {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-600`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.green = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium text-emerald-500 hover:text-white border border-emerald-500 hover:bg-emerald-600`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.yellow = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium text-amber-500 hover:text-white border border-amber-500 hover:bg-amber-600`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.orange = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium text-orange-500 hover:text-white border border-orange-500 hover:bg-orange-600`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.red = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium text-red-500 hover:text-white border border-red-500 hover:bg-red-600`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.purple = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium text-violet-500 hover:text-white border border-violet-500 hover:bg-violet-600`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
+
+LinkButton.dark = ({ className, href, pills, children, ...rest }) => {
+	return (
+		<Link href={href}>
+			<a
+				{...rest}
+				className={`
+				${className ? className + " " : ""}
+				${pills ? "rounded-full" : "rounded"}
+				text-sm transition-all px-3 py-1.5 font-medium text-gray-500 hover:text-white border border-gray-500 hover:bg-gray-600`}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
