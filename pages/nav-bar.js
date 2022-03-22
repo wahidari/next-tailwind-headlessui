bb95ae1bec556cbaee35b4bca6a5eae91df16e29
wahidari <wahiid.ari@gmail.com>
2022-03-22 09:50:38 +0700

    Update nav-bar.js

--- pages/nav-bar.js
+++ pages/nav-bar.js
@@ -9,17 +9,46 @@ import NavbarFromLeft from "@components/NavbarFromLeft";
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { Fragment, useRef, useState } from 'react'
-import { Dialog } from '@headlessui/react'
+import { Dialog, RadioGroup } from '@headlessui/react'
 import { ExclamationIcon } from '@heroicons/react/outline'
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ')
 }
 
+const product = {
+	name: 'Basic Tee 6-Pack ',
+	price: '$192',
+	rating: 3.9,
+	reviewCount: 117,
+	href: '#',
+	imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg',
+	imageAlt: 'Two each of gray, white, and black shirts arranged on table.',
+	colors: [
+		{ name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+		{ name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+		{ name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+	],
+	sizes: [
+		{ name: 'XXS', inStock: true },
+		{ name: 'XS', inStock: true },
+		{ name: 'S', inStock: true },
+		{ name: 'M', inStock: true },
+		{ name: 'L', inStock: true },
+		{ name: 'XL', inStock: true },
+		{ name: 'XXL', inStock: true },
+		{ name: 'XXXL', inStock: false },
+	],
+}
+
 export default function Nav() {
 	const [open, setOpen] = useState(false)
 
 	const cancelButtonRef = useRef(null)
+
+	const [selectedColor, setSelectedColor] = useState(product.colors[0])
+	const [selectedSize, setSelectedSize] = useState(product.sizes[2])
+
 	return (
 		<div>
 			<Head>
@@ -170,6 +199,89 @@ export default function Nav() {
 					</div>
 				</Dialog>
 			</Transition.Root>
+
+			<RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
+				<RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
+				<div className="flex items-center space-x-3">
+					{product.colors.map((color) => (
+						<RadioGroup.Option
+							key={color.name}
+							value={color}
+							className={({ active, checked }) =>
+								classNames(
+									color.selectedClass,
+									active && checked ? 'ring ring-offset-1' : '',
+									!active && checked ? 'ring-2' : '',
+									'-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none'
+								)
+							}
+						>
+							<RadioGroup.Label as="p" className="sr-only">
+								{color.name}
+							</RadioGroup.Label>
+							<span
+								aria-hidden="true"
+								className={classNames(
+									color.class,
+									'h-8 w-8 border border-black border-opacity-10 rounded-full'
+								)}
+							/>
+						</RadioGroup.Option>
+					))}
+				</div>
+			</RadioGroup>
+
+			<RadioGroup value={selectedSize} onChange={setSelectedSize} className="mt-4">
+				<RadioGroup.Label className="sr-only">Choose a size</RadioGroup.Label>
+				<div className="grid grid-cols-4 gap-4">
+					{product.sizes.map((size) => (
+						<RadioGroup.Option
+							key={size.name}
+							value={size}
+							disabled={!size.inStock}
+							className={({ active }) =>
+								classNames(
+									size.inStock
+										? 'bg-white shadow-sm text-gray-900 cursor-pointer'
+										: 'bg-gray-50 text-gray-200 cursor-not-allowed',
+									active ? 'ring-2 ring-indigo-500' : '',
+									'group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1'
+								)
+							}
+						>
+							{({ active, checked }) => (
+								<>
+									<RadioGroup.Label as="p">{size.name}</RadioGroup.Label>
+									{size.inStock ? (
+										<div
+											className={classNames(
+												active ? 'border' : 'border-2',
+												checked ? 'border-indigo-500' : 'border-transparent',
+												'absolute -inset-px rounded-md pointer-events-none'
+											)}
+											aria-hidden="true"
+										/>
+									) : (
+										<div
+											aria-hidden="true"
+											className="absolute -inset-px rounded-md border-2 border-gray-200 pointer-events-none"
+										>
+											<svg
+												className="absolute inset-0 w-full h-full text-gray-200 stroke-2"
+												viewBox="0 0 100 100"
+												preserveAspectRatio="none"
+												stroke="currentColor"
+											>
+												<line x1={0} y1={100} x2={100} y2={0} vectorEffect="non-scaling-stroke" />
+											</svg>
+										</div>
+									)}
+								</>
+							)}
+						</RadioGroup.Option>
+					))}
+				</div>
+			</RadioGroup>
 
 			<main className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
 				<section className="text-gray-600 body-font">
