efbb0ef3d7ffe1638b4652fadbde148a6a2c1acc
wahidari <wahiid.ari@gmail.com>
2022-03-22 10:17:03 +0700

    Update nav-bar.js

--- pages/nav-bar.js
+++ pages/nav-bar.js
@@ -17,13 +17,6 @@ function classNames(...classes) {
 }
 
 const product = {
-	name: 'Basic Tee 6-Pack ',
-	price: '$192',
-	rating: 3.9,
-	reviewCount: 117,
-	href: '#',
-	imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg',
-	imageAlt: 'Two each of gray, white, and black shirts arranged on table.',
 	colors: [
 		{ name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
 		{ name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
