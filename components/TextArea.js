8e02a9119381e7ebec8a7938cd9b57bc699b614c
wahidari <wahiid.ari@gmail.com>
2022-03-24 02:13:13 +0700

    Create TextArea.js

---
+++ components/TextArea.js
@@ -0,0 +1,53 @@
+export default function TextArea({ label, className, id, name, placeholder, value, onChange, height, ...rest }) {
+  return (
+    <div className="mb-4">
+      {label &&
+        <label htmlFor={name} className="block font-medium text-sm text-gray-800 dark:text-white">
+          {label}
+        </label>
+      }
+      <textarea
+        {...rest}
+        id={id}
+        name={name}
+        rows={height}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={`${className ? className + " " : ""}
+          text-sm rounded-md mt-2 w-full transition-all duration-300 p-3
+          dark:text-white bg-white dark:bg-neutral-900 
+          border border-gray-300 dark:border-neutral-700
+          focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none
+        `}
+      />
+    </div>
+  )
+}
+
+TextArea.disabled = ({ label, className, id, name, placeholder, value, onChange, height, ...rest }) => {
+  return (
+    <div className="mb-4">
+      {label &&
+        <label htmlFor={name} className="block font-medium text-sm text-gray-500 dark:text-neutral-300">
+          {label}
+        </label>
+      }
+      <textarea
+        {...rest}
+        id={id}
+        name={name}
+        rows={height}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled
+        className={`${className ? className + " " : ""}
+          text-sm rounded-md mt-2 w-full p-3 cursor-not-allowed
+          text-neutral-500 bg-gray-100 dark:bg-neutral-800
+          border border-gray-300 dark:border-neutral-700
+        `}
+      />
+    </div>
+  )
+}
