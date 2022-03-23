b5da1053d4e6972336ba705b6caf6bdef53604c8
wahidari <wahiid.ari@gmail.com>
2022-03-24 02:13:05 +0700

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,15 +4,9 @@ export default function Avatar({ className, src, alt, border }) {
   return (
     <div className={`
       ${className ? className + " " : ""}
-      relative h-8 w-8 ring-blue-500 ring-2 rounded-full`}
+      relative h-8 w-8 border-blue-500 border-2 rounded-full`}
     >
       <Image src={src} alt={alt} layout="fill" className={`${className ? className + " " : ""}rounded-full`} />
     </div>
-  )
-}
-
-export function Avatars({ item }) {
-  return (
-    <Avatar></Avatar>
   )
 }
