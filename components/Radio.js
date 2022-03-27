98f021c3d915b44d6af169378bbd1998a5a627f9
wahidari <wahiid.ari@gmail.com>
2022-03-27 20:08:27 +0700

    add Radio component

---
+++ components/Radio.js
@@ -0,0 +1,22 @@
+export default function Radio({ label, name, value, onChange, checked, className, ...rest }) {
+  return (
+    <div className="mb-2 text-sm font-medium">
+      <label className="inline-flex items-center cursor-pointer">
+        <input
+          {...rest}
+          name={name}
+          value={value}
+          onChange={onChange}
+          checked={checked}
+          type="radio"
+          className={`
+            ${className ? className + " " : ""}
+          focus:ring-blue-500 h-4 w-4 text-blue-600 dark:bg-neutral-900 dark:checked:bg-blue-500
+            transition-all duration-200 cursor-pointer border-gray-300 dark:border-neutral-700
+					`}
+        />
+        <span className="ml-2 text-gray-700 dark:text-neutral-300">{label}</span>
+      </label>
+    </div>
+  )
+}
