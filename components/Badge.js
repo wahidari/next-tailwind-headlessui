8d672913a4aa18662ccf476617299703a89fc117
wahidari <wahiid.ari@gmail.com>
2022-03-29 15:52:07 +0700

    update Badge components

--- components/Badge.js
+++ components/Badge.js
@@ -1,8 +1,8 @@
-export default function Badge({ className, isLarge, pills, children }) {
+export default function Badge({ className, large, pills, children }) {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
       ${pills ? "rounded-full" : "rounded"}
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] bg-blue-100 text-blue-600`}
     >
@@ -11,11 +11,11 @@ export default function Badge({ className, isLarge, pills, children }) {
   )
 }
 
-Badge.green = ({ className, isLarge, pills, children }) => {
+Badge.green = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
       ${pills ? "rounded-full" : "rounded"}
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] bg-green-100 text-green-600`}
     >
@@ -24,11 +24,11 @@ Badge.green = ({ className, isLarge, pills, children }) => {
   )
 }
 
-Badge.red = ({ className, isLarge, pills, children }) => {
+Badge.red = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
       ${pills ? "rounded-full" : "rounded"}
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] bg-red-100 text-red-600`}
     >
@@ -37,11 +37,11 @@ Badge.red = ({ className, isLarge, pills, children }) => {
   )
 }
 
-Badge.yellow = ({ className, isLarge, pills, children }) => {
+Badge.yellow = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
       ${pills ? "rounded-full" : "rounded"}
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] bg-yellow-100 text-yellow-600`}
     >
@@ -50,11 +50,11 @@ Badge.yellow = ({ className, isLarge, pills, children }) => {
   )
 }
 
-Badge.orange = ({ className, isLarge, pills, children }) => {
+Badge.orange = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
       ${pills ? "rounded-full" : "rounded"}
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] bg-orange-100 text-orange-600`}
     >
@@ -63,11 +63,11 @@ Badge.orange = ({ className, isLarge, pills, children }) => {
   )
 }
 
-Badge.purple = ({ className, isLarge, pills, children }) => {
+Badge.purple = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
       ${pills ? "rounded-full" : "rounded"}
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] bg-violet-100 text-violet-600`}
     >
@@ -76,11 +76,11 @@ Badge.purple = ({ className, isLarge, pills, children }) => {
   )
 }
 
-Badge.dark = ({ className, isLarge, pills, children }) => {
+Badge.dark = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"} 
       ${pills ? "rounded-full" : "rounded"}
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] bg-gray-100 text-gray-600`}
     >
--- components/BadgeOutline.js
+++ components/BadgeOutline.js
@@ -1,8 +1,8 @@
-export default function BadgeOutline({ className, isLarge, pills, children }) {
+export default function BadgeOutline({ className, large, pills, children }) {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
       ${pills ? "rounded-full" : "rounded"} 
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] text-blue-500 border border-blue-500`}
     >
@@ -11,11 +11,11 @@ export default function BadgeOutline({ className, isLarge, pills, children }) {
   )
 }
 
-BadgeOutline.green = ({ className, isLarge, pills, children }) => {
+BadgeOutline.green = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
       ${pills ? "rounded-full" : "rounded"} 
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] text-green-500 border border-green-500`}
     >
@@ -24,11 +24,11 @@ BadgeOutline.green = ({ className, isLarge, pills, children }) => {
   )
 }
 
-BadgeOutline.red = ({ className, isLarge, pills, children }) => {
+BadgeOutline.red = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
       ${pills ? "rounded-full" : "rounded"} 
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] text-red-500 border border-red-500`}
     >
@@ -37,11 +37,11 @@ BadgeOutline.red = ({ className, isLarge, pills, children }) => {
   )
 }
 
-BadgeOutline.yellow = ({ className, isLarge, pills, children }) => {
+BadgeOutline.yellow = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
       ${pills ? "rounded-full" : "rounded"} 
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] text-yellow-500 border border-yellow-500`}
     >
@@ -50,11 +50,11 @@ BadgeOutline.yellow = ({ className, isLarge, pills, children }) => {
   )
 }
 
-BadgeOutline.orange = ({ className, isLarge, pills, children }) => {
+BadgeOutline.orange = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
       ${pills ? "rounded-full" : "rounded"} 
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] text-orange-500 border border-orange-500`}
     >
@@ -63,11 +63,11 @@ BadgeOutline.orange = ({ className, isLarge, pills, children }) => {
   )
 }
 
-BadgeOutline.purple = ({ className, isLarge, pills, children }) => {
+BadgeOutline.purple = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
       ${pills ? "rounded-full" : "rounded"} 
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] text-violet-500 border border-violet-500`}
     >
@@ -76,11 +76,11 @@ BadgeOutline.purple = ({ className, isLarge, pills, children }) => {
   )
 }
 
-BadgeOutline.dark = ({ className, isLarge, pills, children }) => {
+BadgeOutline.dark = ({ className, large, pills, children }) => {
   return (
     <span className={`
       ${className ? className + " " : ""} 
-      ${isLarge ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
+      ${large ? "text-sm !pt-[0.1rem] !pb-[0.1rem]" : "text-xs"}
       ${pills ? "rounded-full" : "rounded"} 
       font-medium px-[0.4rem] pt-[0.2rem] pb-[0.25rem] text-gray-500 dark:text-gray-300 border border-gray-500`}
     >
