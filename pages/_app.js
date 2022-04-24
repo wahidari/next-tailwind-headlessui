b7f0b8edf37c57e1097be2ee016eaad4842e3d70
wahidari <wahiid.ari@gmail.com>
2022-04-24 20:40:36 +0700

    move swiper style

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { GlobalProvider } from "@utils/GlobalContext";
 import { SessionProvider } from "next-auth/react"
 import { AuthProvider } from "@utils/useAuth";
 import "@styles/globals.css";
+import "@styles/swiper.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
 
