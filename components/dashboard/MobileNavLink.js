f159c54d5a6ed24912bef35c0616d2ffcfa459fa
wahidari <wahiid.ari@gmail.com>
2022-05-10 15:45:24 +0700

    Create MobileNavLink.js

---
+++ components/dashboard/MobileNavLink.js
@@ -0,0 +1,12 @@
+import ActiveLink from "@components/ActiveLink";
+
+export default function MobileNavLink({ href = "", children }) {
+
+  return (
+    <ActiveLink activeClassName="bg-gray-100 dark:bg-neutral-800" href={href}>
+      <a className="px-2 block py-1 rounded-md font-medium dark:text-white hover:bg-gray-100 dark:hover:bg-neutral-800 transition-all">
+        {children}
+      </a>
+    </ActiveLink>
+  )
+}
