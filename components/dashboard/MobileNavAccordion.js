30dc7baad5e6465b5e603bb543678bfd46ba4b33
wahidari <wahiid.ari@gmail.com>
2022-05-11 10:10:01 +0700

    add bottom border MobileNavAccordion

--- components/dashboard/MobileNavAccordion.js
+++ components/dashboard/MobileNavAccordion.js
@@ -29,23 +29,26 @@ export default function MobileNavAccordion({ name, routeName, children }) {
 
   return (
     cek ?
-      <Disclosure defaultOpen={isOpen}>
-        {({ open }) => (
-          <>
-            <Disclosure.Button className="w-full flex justify-between px-2 py-1 items-center text-sm font-medium dark:text-white hover:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 rounded transition-all">
-              <span>{name}</span>
-              <ChevronRightIcon
-                className={`w-4 h-4
+      <>
+        <Disclosure defaultOpen={isOpen}>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="w-full flex justify-between px-2 py-1 items-center text-sm font-medium dark:text-white hover:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 rounded transition-all">
+                <span>{name}</span>
+                <ChevronRightIcon
+                  className={`w-4 h-4
                   ${open ? 'transform rotate-90 transition-transform' : 'transition-transform'} 
                 `}
-              />
-            </Disclosure.Button>
-            <Disclosure.Panel className="px-3 space-y-1">
-              {children}
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 space-y-1">
+                {children}
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <hr className="border-neutral-200 dark:border-neutral-800" />
+      </>
       : ""
   )
 }
