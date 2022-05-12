b07230f25b6c24da3282e36c611efd13ed22afb1
wahidari <wahiid.ari@gmail.com>
2022-05-12 15:35:09 +0700

    update LayoutDashboard component

--- components/dashboard/LayoutDashboard.js
+++ components/dashboard/LayoutDashboard.js
@@ -1,13 +1,26 @@
+import Breadcrumb from "@components/Breadcrumb";
+import DashboardNav from "@components/dashboard/DashboardNav"
+import MobileMenu from "@components/dashboard//MobileMenu";
+import SidebarMenu from "@components/dashboard//SidebarMenu";
+import ThemeToggle from "@components/dashboard//ThemeToggle";
+
 export default function LayoutDashboard({ children }) {
   return (
-    // if using custom scrollbar 
-    // <div className="dark:bg-neutral-900 min-h-screen w-screen">
-    <div className="dark:bg-neutral-900">
-      {/* "auto" width sidebar and take the rest for main content "1fr" */}
-      {/* https://developer.mozilla.org/en-US/docs/Web/CSS/grid-template-columns */}
-      <div className="min-h-screen w-screen xl:grid" style={{ gridTemplateColumns: "auto 1fr" }}>
-        {children}
-      </div>
+    <div className="dark:bg-neutral-900 min-h-screen">
+      <ThemeToggle />
+      <DashboardNav />
+      <MobileMenu />
+      <div className="max-w-screen-2xl mx-auto px-2 mt-2 border dark:border-neutral-700 h-full">
+        <div className="lg:flex py-2 h-full">
+          <SidebarMenu />
+          <main className="px-2 w-full border dark:border-neutral-700 lg:ml-60">
+            <Breadcrumb />
+            {/* Start Content */}
+            {children}
+            {/* End Content */}
+          </main>
+        </div>
+      </div >
     </div>
   );
 }
