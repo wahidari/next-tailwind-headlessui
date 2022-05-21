d2eb09825305c1b0e98427fc897c48fc834a3f16
wahidari <wahiid.ari@gmail.com>
2022-05-21 16:28:18 +0700

    update Accordion component

--- components/Accordion.js
+++ components/Accordion.js
@@ -6,7 +6,7 @@ export default function Accordion({ title, children, className }) {
 		<Disclosure as="div" className={`${className ? className + " " : " "} my-1 text-sm`}>
 			{({ open }) => (
 				<>
-					<Disclosure.Button className="flex justify-between w-full px-4 py-2 font-medium text-left text-blue-500 bg-gray-100 dark:bg-neutral-800 rounded-lg hover:bg-gray-200 dark:hover:bg-neutral-700">
+					<Disclosure.Button className="flex justify-between w-full px-4 py-2 font-medium text-left text-blue-500 bg-gray-100 dark:bg-neutral-800 rounded-lg hover:bg-gray-200 dark:hover:bg-neutral-700 transition-all">
 						<span>{title}</span>
 						<ChevronUpIcon
 							className={`${open ? 'transform rotate-180 duration-300' : 'transform rotate-0 duration-200'} w-5 h-5 text-blue-500`}
