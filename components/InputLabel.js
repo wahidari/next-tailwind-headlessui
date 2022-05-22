5600f65e47ec52ed07c0d890806d55130498b994
wahidari <wahiid.ari@gmail.com>
2022-05-22 20:18:12 +0700

    update InputLabel component

--- components/InputLabel.js
+++ components/InputLabel.js
@@ -23,7 +23,7 @@ export default function InputLabel({ id, className, label, type, name, placehold
           className={`
             ${className ? className + " " : ""} 
             ${labelLeft ? "rounded-r-md" : "rounded-l-md"}
-            text-sm transition-all w-full px-3 py-[0.6rem] z-10
+            text-sm transition-all w-full px-3 py-[0.6rem]
             dark:text-white bg-white dark:bg-neutral-900  
             border border-gray-300 dark:border-neutral-700 
             focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none
@@ -39,7 +39,7 @@ export default function InputLabel({ id, className, label, type, name, placehold
   );
 }
 
-InputLabel.disabled = ({ id, className, label, type, name, placeholder, value, onChange, labelLeft, ...rest }) => {
+InputLabel.disabled = ({ id, className, label, type, name, placeholder, value, onChange, labelLeft, labelRight, ...rest }) => {
   return (
     <div className="mb-4">
       {label &&
@@ -47,22 +47,36 @@ InputLabel.disabled = ({ id, className, label, type, name, placeholder, value, o
           {label}
         </label>
       }
-      <input
-        {...rest}
-        id={id}
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        disabled
-        className={`
-          ${className ? className + " " : ""}
-          text-sm w-full px-3 py-[0.6rem] rounded-md mt-2 cursor-not-allowed
-          text-neutral-500 bg-gray-100 dark:bg-neutral-800
-          border border-gray-300 dark:border-neutral-700
-        `}
-      />
+      <div className="flex rounded-md items-center mt-2">
+        {labelLeft &&
+          <span className="inline-flex items-center px-3 h-[2.58rem] rounded-l-md border border-r-0 border-gray-300 dark:border-neutral-700 bg-gray-100 dark:bg-neutral-800 text-gray-500 dark:text-gray-400 text-sm">
+            {labelLeft}
+          </span>
+        }
+        <input
+          {...rest}
+          id={id}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          disabled
+          className={`
+            ${className ? className + " " : ""} 
+            ${labelLeft ? "rounded-r-md" : "rounded-l-md"}
+            text-sm transition-all w-full px-3 py-[0.6rem]
+            dark:text-white bg-white dark:bg-neutral-800  
+            border border-gray-300 dark:border-neutral-700 
+            cursor-not-allowed
+          `}
+        />
+        {labelRight &&
+          <span className="inline-flex items-center px-3 h-[2.58rem] rounded-r-md border border-l-0 border-gray-300 dark:border-neutral-700 bg-gray-100 dark:bg-neutral-800 text-gray-500 dark:text-gray-400 text-sm">
+            {labelRight}
+          </span>
+        }
+      </div>
     </div>
   );
 };
